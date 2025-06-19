047ed62f65d3c66182e1712e0772ee3a8b414fca
Adrian Corscadden <me@acorscadden.com>
2025-06-18 17:19:46 -0700

    Update response data format

--- Sources/OpenMeteoKit/OpenMeteoClient.swift
+++ Sources/OpenMeteoKit/OpenMeteoClient.swift
@@ -82,7 +82,7 @@ public enum OpenMeteoError: Error {
 
 // MARK: - Response Models
 
-public struct WeatherResponse: Codable {
+public struct WeatherResponse: Decodable {
   public let latitude: Double
   public let longitude: Double
   public let generationTimeMs: Double
@@ -90,96 +90,117 @@ public struct WeatherResponse: Codable {
   public let timezone: String
   public let timezoneAbbreviation: String
   public let elevation: Double
-  public let hourlyUnits: HourlyUnits
-  public let hourly: HourlyData
+  public let hourly: [HourlyData]
 
   enum CodingKeys: String, CodingKey {
-    case latitude, longitude, timezone, elevation, hourly
+    case latitude, longitude, timezone, elevation
     case generationTimeMs = "generationtime_ms"
     case utcOffsetSeconds = "utc_offset_seconds"
     case timezoneAbbreviation = "timezone_abbreviation"
     case hourlyUnits = "hourly_units"
-  }
-}
-
-public struct HourlyUnits: Codable {
-  public let time: String
-  public let windSpeed10mEcmwfIfs025: String?
-  public let windDirection10mEcmwfIfs025: String?
-  public let windGusts10mEcmwfIfs025: String?
-  public let windSpeed10mIconSeamless: String?
-  public let windDirection10mIconSeamless: String?
-  public let windGusts10mIconSeamless: String?
-
-  enum CodingKeys: String, CodingKey {
-    case time
-    case windSpeed10mEcmwfIfs025 = "wind_speed_10m_ecmwf_ifs025"
-    case windDirection10mEcmwfIfs025 = "wind_direction_10m_ecmwf_ifs025"
-    case windGusts10mEcmwfIfs025 = "wind_gusts_10m_ecmwf_ifs025"
-    case windSpeed10mIconSeamless = "wind_speed_10m_icon_seamless"
-    case windDirection10mIconSeamless = "wind_direction_10m_icon_seamless"
-    case windGusts10mIconSeamless = "wind_gusts_10m_icon_seamless"
-  }
-}
-
-public struct HourlyData: Codable {
-  public let time: [String]
-  public let windSpeed10mEcmwfIfs025: [Double]?
-  public let windDirection10mEcmwfIfs025: [Int]?
-  public let windGusts10mEcmwfIfs025: [Double]?
-  public let windSpeed10mIconSeamless: [Double]?
-  public let windDirection10mIconSeamless: [Int]?
-  public let windGusts10mIconSeamless: [Double]?
-
-  enum CodingKeys: String, CodingKey {
-    case time
-    case windSpeed10mEcmwfIfs025 = "wind_speed_10m_ecmwf_ifs025"
-    case windDirection10mEcmwfIfs025 = "wind_direction_10m_ecmwf_ifs025"
-    case windGusts10mEcmwfIfs025 = "wind_gusts_10m_ecmwf_ifs025"
-    case windSpeed10mIconSeamless = "wind_speed_10m_icon_seamless"
-    case windDirection10mIconSeamless = "wind_direction_10m_icon_seamless"
-    case windGusts10mIconSeamless = "wind_gusts_10m_icon_seamless"
+    case hourlyData = "hourly"
   }
 
-  public var weatherDataPoints: [WeatherDataPoint] {
-    var points: [WeatherDataPoint] = []
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
 
-    for (index, timeString) in time.enumerated() {
-      let ecmwfData = WeatherModelData(
-        windSpeed: windSpeed10mEcmwfIfs025?[safe: index],
-        windDirection: windDirection10mEcmwfIfs025?[safe: index],
-        windGusts: windGusts10mEcmwfIfs025?[safe: index]
-      )
+    latitude = try container.decode(Double.self, forKey: .latitude)
+    longitude = try container.decode(Double.self, forKey: .longitude)
+    generationTimeMs = try container.decode(Double.self, forKey: .generationTimeMs)
+    utcOffsetSeconds = try container.decode(Int.self, forKey: .utcOffsetSeconds)
+    timezone = try container.decode(String.self, forKey: .timezone)
+    timezoneAbbreviation = try container.decode(String.self, forKey: .timezoneAbbreviation)
+    elevation = try container.decode(Double.self, forKey: .elevation)
 
-      let iconData = WeatherModelData(
-        windSpeed: windSpeed10mIconSeamless?[safe: index],
-        windDirection: windDirection10mIconSeamless?[safe: index],
-        windGusts: windGusts10mIconSeamless?[safe: index]
-      )
+    let units = try container.decode(HourlyUnits.self, forKey: .hourlyUnits)
+    let data = try container.decode(RawHourlyData.self, forKey: .hourlyData)
 
-      let point = WeatherDataPoint(
-        time: timeString,
-        ecmwfIfs025: ecmwfData,
-        iconSeamless: iconData
-      )
-
-      points.append(point)
-    }
-
-    return points
+    // Transform raw data into structured hourly data
+    hourly = Self.transformHourlyData(from: data, units: units)
   }
-}
 
-public struct WeatherDataPoint {
-  public let time: String
-  public let ecmwfIfs025: WeatherModelData
-  public let iconSeamless: WeatherModelData
-
-  public var date: Date? {
+  private static func transformHourlyData(from data: RawHourlyData, units: HourlyUnits) -> [HourlyData] {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd'T'HH:mm"
     formatter.timeZone = TimeZone(abbreviation: "GMT")
-    return formatter.date(from: time)
+
+    return data.time.enumerated().map { index, timeString in
+      let date = formatter.date(from: timeString) ?? Date()
+
+      var modelData: [WeatherModel: WeatherModelData] = [:]
+
+      // ECMWF IFS025 data
+      modelData[.ecmwfIfs025] = WeatherModelData(
+        windSpeed: data.windSpeed10mEcmwfIfs025?[safe: index],
+        windDirection: data.windDirection10mEcmwfIfs025?[safe: index],
+        windGusts: data.windGusts10mEcmwfIfs025?[safe: index],
+        windSpeedUnit: units.windSpeed10mEcmwfIfs025 ?? "kn",
+        windDirectionUnit: units.windDirection10mEcmwfIfs025 ?? "°",
+        windGustsUnit: units.windGusts10mEcmwfIfs025 ?? "kn"
+      )
+
+      // Icon Seamless data
+      modelData[.iconSeamless] = WeatherModelData(
+        windSpeed: data.windSpeed10mIconSeamless?[safe: index],
+        windDirection: data.windDirection10mIconSeamless?[safe: index],
+        windGusts: data.windGusts10mIconSeamless?[safe: index],
+        windSpeedUnit: units.windSpeed10mIconSeamless ?? "kn",
+        windDirectionUnit: units.windDirection10mIconSeamless ?? "°",
+        windGustsUnit: units.windGusts10mIconSeamless ?? "kn"
+      )
+
+      return HourlyData(time: date, models: modelData)
+    }
+  }
+}
+
+private struct HourlyUnits: Decodable {
+  let time: String
+  let windSpeed10mEcmwfIfs025: String?
+  let windDirection10mEcmwfIfs025: String?
+  let windGusts10mEcmwfIfs025: String?
+  let windSpeed10mIconSeamless: String?
+  let windDirection10mIconSeamless: String?
+  let windGusts10mIconSeamless: String?
+
+  enum CodingKeys: String, CodingKey {
+    case time
+    case windSpeed10mEcmwfIfs025 = "wind_speed_10m_ecmwf_ifs025"
+    case windDirection10mEcmwfIfs025 = "wind_direction_10m_ecmwf_ifs025"
+    case windGusts10mEcmwfIfs025 = "wind_gusts_10m_ecmwf_ifs025"
+    case windSpeed10mIconSeamless = "wind_speed_10m_icon_seamless"
+    case windDirection10mIconSeamless = "wind_direction_10m_icon_seamless"
+    case windGusts10mIconSeamless = "wind_gusts_10m_icon_seamless"
+  }
+}
+
+private struct RawHourlyData: Decodable {
+  let time: [String]
+  let windSpeed10mEcmwfIfs025: [Double]?
+  let windDirection10mEcmwfIfs025: [Int]?
+  let windGusts10mEcmwfIfs025: [Double]?
+  let windSpeed10mIconSeamless: [Double]?
+  let windDirection10mIconSeamless: [Int]?
+  let windGusts10mIconSeamless: [Double]?
+
+  enum CodingKeys: String, CodingKey {
+    case time
+    case windSpeed10mEcmwfIfs025 = "wind_speed_10m_ecmwf_ifs025"
+    case windDirection10mEcmwfIfs025 = "wind_direction_10m_ecmwf_ifs025"
+    case windGusts10mEcmwfIfs025 = "wind_gusts_10m_ecmwf_ifs025"
+    case windSpeed10mIconSeamless = "wind_speed_10m_icon_seamless"
+    case windDirection10mIconSeamless = "wind_direction_10m_icon_seamless"
+    case windGusts10mIconSeamless = "wind_gusts_10m_icon_seamless"
+  }
+}
+
+public struct HourlyData {
+  public let time: Date
+  public let models: [WeatherModel: WeatherModelData]
+
+  // Convenience accessors for each model
+  public subscript(model: WeatherModel) -> WeatherModelData? {
+    return models[model]
   }
 }
 
@@ -187,6 +208,9 @@ public struct WeatherModelData {
   public let windSpeed: Double?
   public let windDirection: Int?
   public let windGusts: Double?
+  public let windSpeedUnit: String
+  public let windDirectionUnit: String
+  public let windGustsUnit: String
 }
 
 private extension Array {
--- Tests/OpenMeteoKitTests/OpenMeteoKitTests.swift
+++ Tests/OpenMeteoKitTests/OpenMeteoKitTests.swift
@@ -65,16 +65,33 @@ import Foundation
   #expect(response.timezone == "GMT")
   #expect(response.elevation == 38.0)
 
-  #expect(response.hourlyUnits.time == "iso8601")
-  #expect(response.hourlyUnits.windSpeed10mEcmwfIfs025 == "kn")
+  // Test the new hourly array structure
+  #expect(response.hourly.count == 2)
 
-  #expect(response.hourly.time.count == 2)
-  #expect(response.hourly.time[0] == "2025-06-18T00:00")
-  #expect(response.hourly.windSpeed10mEcmwfIfs025?[0] == 3.8)
-  #expect(response.hourly.windDirection10mEcmwfIfs025?[0] == 240)
+  // Test first hourly data point
+  let firstHourly = response.hourly[0]
+  #expect(firstHourly.time != Date(timeIntervalSince1970: 0)) // Should have proper date
+
+  // Test ECMWF model data access
+  let ecmwfData = firstHourly[.ecmwfIfs025]
+  #expect(ecmwfData != nil)
+  #expect(ecmwfData?.windSpeed == 3.8)
+  #expect(ecmwfData?.windDirection == 240)
+  #expect(ecmwfData?.windGusts == 7.0)
+  #expect(ecmwfData?.windSpeedUnit == "kn")
+  #expect(ecmwfData?.windDirectionUnit == "°")
+  #expect(ecmwfData?.windGustsUnit == "kn")
+
+  // Test Icon model data access
+  let iconData = firstHourly[.iconSeamless]
+  #expect(iconData != nil)
+  #expect(iconData?.windSpeed == 1.6)
+  #expect(iconData?.windDirection == 256)
+  #expect(iconData?.windGusts == 2.9)
+  #expect(iconData?.windSpeedUnit == "kn")
 }
 
-@Test func testWeatherDataPointsGeneration() throws {
+@Test func testHourlyDataModelAccess() throws {
   let jsonString = """
   {
     "latitude": 52.5,
@@ -112,33 +129,71 @@ import Foundation
   let decoder = JSONDecoder()
   let response = try decoder.decode(WeatherResponse.self, from: jsonData)
 
-  let dataPoints = response.hourly.weatherDataPoints
+  #expect(response.hourly.count == 2)
 
-  #expect(dataPoints.count == 2)
+  let firstHourly = response.hourly[0]
+  let secondHourly = response.hourly[1]
 
-  let firstPoint = dataPoints[0]
-  #expect(firstPoint.time == "2025-06-18T00:00")
-  #expect(firstPoint.ecmwfIfs025.windSpeed == 3.8)
-  #expect(firstPoint.ecmwfIfs025.windDirection == 240)
-  #expect(firstPoint.ecmwfIfs025.windGusts == 7.0)
-  #expect(firstPoint.iconSeamless.windSpeed == 1.6)
-  #expect(firstPoint.iconSeamless.windDirection == 256)
-  #expect(firstPoint.iconSeamless.windGusts == 2.9)
+  // Test first data point
+  let firstEcmwf = firstHourly[.ecmwfIfs025]
+  #expect(firstEcmwf?.windSpeed == 3.8)
+  #expect(firstEcmwf?.windDirection == 240)
+  #expect(firstEcmwf?.windGusts == 7.0)
+
+  let firstIcon = firstHourly[.iconSeamless]
+  #expect(firstIcon?.windSpeed == 1.6)
+  #expect(firstIcon?.windDirection == 256)
+  #expect(firstIcon?.windGusts == 2.9)
+
+  // Test second data point
+  let secondEcmwf = secondHourly[.ecmwfIfs025]
+  #expect(secondEcmwf?.windSpeed == 3.8)
+  #expect(secondEcmwf?.windDirection == 249)
+  #expect(secondEcmwf?.windGusts == 7.6)
+
+  let secondIcon = secondHourly[.iconSeamless]
+  #expect(secondIcon?.windSpeed == 2.5)
+  #expect(secondIcon?.windDirection == 274)
+  #expect(secondIcon?.windGusts == 4.9)
 }
 
-@Test func testWeatherDataPointDateParsing() {
-  let dataPoint = WeatherDataPoint(
-    time: "2025-06-18T12:30",
-    ecmwfIfs025: WeatherModelData(windSpeed: 10.0, windDirection: 180, windGusts: 15.0),
-    iconSeamless: WeatherModelData(windSpeed: 9.5, windDirection: 175, windGusts: 14.5)
-  )
+@Test func testDateParsing() throws {
+  let jsonString = """
+  {
+    "latitude": 52.5,
+    "longitude": 13.5,
+    "generationtime_ms": 0.16367435455322266,
+    "utc_offset_seconds": 0,
+    "timezone": "GMT",
+    "timezone_abbreviation": "GMT",
+    "elevation": 38.0,
+    "hourly_units": {
+      "time": "iso8601",
+      "wind_speed_10m_ecmwf_ifs025": "kn",
+      "wind_direction_10m_ecmwf_ifs025": "°",
+      "wind_gusts_10m_ecmwf_ifs025": "kn"
+    },
+    "hourly": {
+      "time": [
+        "2025-06-18T12:30"
+      ],
+      "wind_speed_10m_ecmwf_ifs025": [10.0],
+      "wind_direction_10m_ecmwf_ifs025": [180],
+      "wind_gusts_10m_ecmwf_ifs025": [15.0]
+    }
+  }
+  """
 
-  let date = dataPoint.date
-  #expect(date != nil)
+  let jsonData = jsonString.data(using: .utf8)!
+  let decoder = JSONDecoder()
+  let response = try decoder.decode(WeatherResponse.self, from: jsonData)
+
+  let hourlyData = response.hourly[0]
+  let date = hourlyData.time
 
   var calendar = Calendar(identifier: .gregorian)
   calendar.timeZone = TimeZone(abbreviation: "GMT")!
-  let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date!)
+  let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
 
   #expect(components.year == 2025)
   #expect(components.month == 6)
